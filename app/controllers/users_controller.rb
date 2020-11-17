462b0081d3ae78f00961afad24acc339e84b1781
Rachel Moser <moser.rachel@gmail.com>
2020-11-17 17:07:44 -0600

    update form using rails

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
 
   def create
-    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
   
     if @user.save
